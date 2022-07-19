94a4731634a72baa732efec5fa59eac6e64076f0
joan17cast <joan17cast@gmail.com>
2022-07-19 20:01:55 +0200

    Added routes to bouth pages

---
+++ src/pages/NewsDetail.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function NewsDetail() {
+  return (
+    <SafeAreaView>
+      <Text>NewsDetail</Text>
+    </SafeAreaView>
+  );
+}
