7e30d4a2be38ef3a84e9c37ac973fa5e89c02649
joan17cast <joan17cast@gmail.com>
2022-07-20 20:16:22 +0200

    Added Home feed and Component FeedItem

---
+++ src/components/FeedItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {useAtom} from 'jotai';
+import {Pressable, Text, Image, NativeModules, View} from 'react-native';
+import {newsAtom} from '../jotai/newsAtom';
+import moment from 'moment';
+export default function FeedItem(props) {
+  const [stNewsAtom, setNewsAtom] = useAtom(newsAtom);
+  function getImage(desc) {
+    const match = desc?.match(/\bhttps?:\/\/\S+/gi);
+    console.log(props.itemInfo);
+    console.log(match[0].replace('"/', '').replace('"', ''));
+    return match[0].replace('"/', '').replace('"', '').replace('>', '');
+  }
+  const locale = NativeModules.I18nManager.localeIdentifier;
+  moment.locale(locale.substring(2));
+  return (
+    <Pressable
+      onPress={() => {
+        props.onPress(),
+          setNewsAtom({...stNewsAtom, html: props.itemInfo.description});
+      }}
+      style={{borderBottomWidth: 1}}>
+      <Text style={{padding: 5}}>{props.itemInfo?.title}</Text>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          style={{width: 150, height: 150}}
+          source={{
+            uri: getImage(props.itemInfo?.description),
+          }}
+        />
+      </View>
+      <Text style={{padding: 5}}>
+        {moment(props.itemInfo?.created).format('LLL')}
+      </Text>
+    </Pressable>
+  );
+}
--- src/pages/HomeFeed.js
+++ src/pages/HomeFeed.js
@@ -1,18 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, FlatList} from 'react-native';
 
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {parse} from 'rss-to-json';
 
-export default function HomeFeed() {
+// * Components
+import FeedItem from '../components/FeedItem';
+
+const initState ={
+  loading: false
+}
+
+export default function HomeFeed({ navigation }) {
+  const [state, setState] = useState(initState);
+  useEffect(() => {
+    getNewsFeed();
+  }, []);
+
+  async function getNewsFeed() {
+    setState({...state, loading: true});
+    const rss = await parse('http://www.xatakandroid.com/tag/feeds/rss2.xml');
+    setState({...state, items: rss.items, loading: false});
+    console.log(rss);
+  }
   return (
     <SafeAreaView>
-      <Text>HomeFeed</Text>
+      <FlatList
+        refreshing={state.loading}
+        onRefresh={getNewsFeed}
+        data={state?.items}
+        renderItem={({item}) => <FeedItem onPress={() => {navigation.navigate("News")}} itemInfo={item}/>}
+      />
     </SafeAreaView>
   );
 }
